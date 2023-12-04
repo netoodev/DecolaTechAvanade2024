d2fb341a15643caffb9fb89ca94782fedbd085e0
netoodev <netoodeveloper@gmail.com>
2023-12-04 18:26:41 -0300

    Interrompendo o fluxo de execução

--- exemplo-fundamentos/Program.cs
+++ exemplo-fundamentos/Program.cs
@@ -8,6 +8,11 @@ while (contador <= 10)
 {
     Console.WriteLine($"{contador}° execução: {numero} x {contador} = {numero * contador}");
     contador++;
+
+    if (contador == 5)
+    {
+        break;
+    }
 }
 
 
