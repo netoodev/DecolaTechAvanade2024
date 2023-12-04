d6fd458fe36020850ade23f7b260b195ae96303b
netoodev <netoodeveloper@gmail.com>
2023-12-04 18:21:29 -0300

    Introdução ao WHILE

--- exemplo-fundamentos/Program.cs
+++ exemplo-fundamentos/Program.cs
@@ -1,11 +1,28 @@
 ﻿using exemplo_fundamentos.Models;
 
-int numero = 5;
 
-for(int contador = 0; contador <= 10; contador++) 
+int numero = 5;
+int contador = 0;
+
+while (contador <= 10)
 {
-    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+    Console.WriteLine($"{contador}° execução: {numero} x {contador} = {numero * contador}");
+    contador++;
 }
+
+
+
+
+
+
+
+
+// int numero = 5;
+
+// for(int contador = 0; contador <= 10; contador++) 
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
 
 
 
