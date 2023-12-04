7de3c9740a1e6be9006f95ee58e9fc0f78007294
netoodev <netoodeveloper@gmail.com>
2023-12-04 15:31:05 -0300

    Funções Trigonométricas

--- exemplo-fundamentos/Program.cs
+++ exemplo-fundamentos/Program.cs
@@ -7,7 +7,11 @@ Calculadora calc = new Calculadora();
 // calc.Subtrair(10,50);
 // calc.Multiplicar(15,45);
 // calc.Dividir(2,2);
-calc.Potencia(3,3);
+// calc.Potencia(3,3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+
 
 
 
