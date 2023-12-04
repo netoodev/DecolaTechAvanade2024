d7f33288b1a05565fb55ebb6b6f3b734ba78c632
netoodev <netoodeveloper@gmail.com>
2023-12-04 15:15:49 -0300

    Usando potência

--- exemplo-fundamentos/Models/Calculadora.cs
+++ exemplo-fundamentos/Models/Calculadora.cs
@@ -27,5 +27,11 @@ namespace exemplo_fundamentos.Models
             Console.WriteLine($"{x} / {y} = {x / y}");
         }
 
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x,y);
+            Console.WriteLine($"{x} ^ {y} = {pot}");
+        }
+
     }
 }
--- exemplo-fundamentos/Program.cs
+++ exemplo-fundamentos/Program.cs
@@ -3,10 +3,11 @@
 
 Calculadora calc = new Calculadora();
 
-calc.Somar(10,30);
-calc.Subtrair(10,50);
-calc.Multiplicar(15,45);
-calc.Dividir(2,2);
+// calc.Somar(10,30);
+// calc.Subtrair(10,50);
+// calc.Multiplicar(15,45);
+// calc.Dividir(2,2);
+calc.Potencia(3,3);
 
 
 
