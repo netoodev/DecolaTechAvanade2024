4a4d7f4a5357f7b10e033de882c8a5840fe12a23
netoodev <netoodeveloper@gmail.com>
2023-12-06 10:42:13 -0300

    Implementando um array de inteiros

--- exemplo-fundamentos/Program.cs
+++ exemplo-fundamentos/Program.cs
@@ -1,46 +1,66 @@
 ﻿using exemplo_fundamentos.Models;
 
-string? opcao;
-bool exibirMenu = true; 
+int[] arrayInteiros = new int[3];
 
-while (exibirMenu)
-{ 
-    Console.Clear();
-    Console.WriteLine("Digite a sua opção: ");
-    Console.WriteLine("1 - Cadastrar cliente");
-    Console.WriteLine("2 -Buscar cliente");
-    Console.WriteLine("3 - Apagar cliente");
-    Console.WriteLine("4 - Encerrar");
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 74;
+arrayInteiros[2] = 50;
+// arrayInteiros[3] = 1;
 
-    opcao = Console.ReadLine();
-
-    switch (opcao)
-    {
-        case "1":
-            Console.WriteLine("Cadastro de cliente"); 
-            break;
-            
-        case "2":
-            Console.WriteLine("Buscar cliente"); 
-            break;
-
-        case "3":
-            Console.WriteLine("Apagar cliente"); 
-            break;
-        
-        case "4":
-            Console.WriteLine("Encerrar");
-            exibirMenu = false;
-            break;
-
-        default:
-            Console.WriteLine("Opção inválida");
-            break;
-            
-    }
+for(int contador = 0; contador < arrayInteiros.Length; contador++)
+{
+    Console.WriteLine($"Posição N° {contador} - {arrayInteiros[contador]}");
 }
 
-Console.WriteLine("O seu programa se encerrou.");
+
+
+
+
+
+
+
+
+// string? opcao;
+// bool exibirMenu = true; 
+
+// while (exibirMenu)
+// { 
+//     Console.Clear();
+//     Console.WriteLine("Digite a sua opção: ");
+//     Console.WriteLine("1 - Cadastrar cliente");
+//     Console.WriteLine("2 -Buscar cliente");
+//     Console.WriteLine("3 - Apagar cliente");
+//     Console.WriteLine("4 - Encerrar");
+
+//     opcao = Console.ReadLine();
+
+//     switch (opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("Cadastro de cliente"); 
+//             break;
+            
+//         case "2":
+//             Console.WriteLine("Buscar cliente"); 
+//             break;
+
+//         case "3":
+//             Console.WriteLine("Apagar cliente"); 
+//             break;
+        
+//         case "4":
+//             Console.WriteLine("Encerrar");
+//             exibirMenu = false;
+//             break;
+
+//         default:
+//             Console.WriteLine("Opção inválida");
+//             break;
+            
+//     }
+// }
+
+// Console.WriteLine("O seu programa se encerrou.");
 
 
 
