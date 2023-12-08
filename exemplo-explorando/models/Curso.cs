48201ef0b187589bd150246d85599d900cef9aa2
Neto <netoodeveloper@gmail.com>
2023-12-08 02:27:03 -0300

    Concatenando Strings

--- exemplo-explorando/models/Curso.cs
+++ exemplo-explorando/models/Curso.cs
@@ -29,10 +29,14 @@ namespace exemplo_explorando.models
         public void ListarAlunos()
         {
             Console.WriteLine($"Alunos do curso de: {Nome}");
-            foreach (Pessoa aluno in Alunos)
+
+            for (int count = 0; count < Alunos.Count; count++)
             {
-                Console.WriteLine(aluno.NomeCompleto);
+                string texto = "N° " + count + " - " + Alunos[count].NomeCompleto;
+
+                Console.WriteLine(texto);
             }
+
         }
     }
 }
