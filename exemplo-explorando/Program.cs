57fddd499faf246f4485a6a7e40276d99b3e2693
Neto <netoodeveloper@gmail.com>
2023-12-10 15:44:17 -0300

    Pilha na prática

--- exemplo-explorando/Program.cs
+++ exemplo-explorando/Program.cs
@@ -1,25 +1,62 @@
 ﻿using exemplo_explorando.models;
+using System.Collections;
 using System.Globalization;
 
-Queue<int> fila = new Queue<int>();
 
-fila.Enqueue(2);
-fila.Enqueue(4);
-fila.Enqueue(6);
-fila.Enqueue(8);
+Stack<int> pilha = new Stack<int>();
 
-foreach (int item in fila)
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+
+
+foreach (int item in pilha)
 {
     Console.WriteLine(item);
 }
 
-Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
-fila.Enqueue(10);
+Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
 
-foreach (int item in fila)
+pilha.Push(20);
+
+foreach (int item in pilha)
 {
     Console.WriteLine(item);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Queue<int> fila = new Queue<int>();
+
+// fila.Enqueue(2);
+// fila.Enqueue(4);
+// fila.Enqueue(6);
+// fila.Enqueue(8);
+
+// foreach (int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+// fila.Enqueue(10);
+
+// foreach (int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
 
 
 
