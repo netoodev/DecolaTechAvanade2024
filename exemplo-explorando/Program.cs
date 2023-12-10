ce7d5a2b61be1e06dba0c58867938e772b1b7905
Neto <netoodeveloper@gmail.com>
2023-12-10 15:00:21 -0300

    Exceção genérica e específica

--- exemplo-explorando/Program.cs
+++ exemplo-explorando/Program.cs
@@ -12,7 +12,19 @@ foreach (string linha in linhas)
     Console.WriteLine(linha);
 }
 
-} catch(Exception ex)
+} 
+
+catch(FileNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
+}
+
+catch(DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado. {ex.Message}");
+}
+
+catch(Exception ex)
 {
     Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
 }
