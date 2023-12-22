ca9c741eeb38b59f8e02fab998f3b78236eede24
Neto <netoodeveloper@gmail.com>
2023-12-22 20:06:04 -0300

    Método selado na prática

--- exemplo-poo/Models/Diretor.cs
+++ exemplo-poo/Models/Diretor.cs
@@ -7,6 +7,9 @@ namespace exemplo_poo.Models
 {
     public class Diretor : Professor
     {
-        
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome},  tenho {Idade} anos, sou o Diretor e ganho {Salario:C}.");
+        }
     }
 }
