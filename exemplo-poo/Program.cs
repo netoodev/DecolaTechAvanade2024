07709777fff211a7b3cde040968fa91d808516a9
Neto <netoodeveloper@gmail.com>
2023-12-22 01:45:53 -0300

    Encapsulamento na prática

--- exemplo-poo/Program.cs
+++ exemplo-poo/Program.cs
@@ -1,7 +1,31 @@
 ﻿using exemplo_poo.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Neto";
-p1.Idade = 19;
+ContaCorrente c1 = new ContaCorrente(123, 1000);
 
-p1.Apresentar();
+c1.ExibirSaldo();
+c1.Sacar(5874);
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Neto";
+// p1.Idade = 19;
+
+// p1.Apresentar();
