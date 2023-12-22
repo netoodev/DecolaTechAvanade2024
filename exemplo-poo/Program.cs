31166c0ec216ffb90afe4cf44f67c84b3f4efbfe
Neto <netoodeveloper@gmail.com>
2023-12-22 17:20:43 -0300

    Herança na prática

--- exemplo-poo/Program.cs
+++ exemplo-poo/Program.cs
@@ -1,10 +1,42 @@
 ﻿using exemplo_poo.Models;
 
-ContaCorrente c1 = new ContaCorrente(123, 1000);
+Aluno a1 = new Aluno();
 
-c1.ExibirSaldo();
-c1.Sacar(5874);
-c1.ExibirSaldo();
+a1.Nome = "Ana Maria";
+a1.Idade = 48;
+a1.Email = "anamaria@edu.pe.avanade.br";
+a1.Nota = 8.5;
+a1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+// c1.ExibirSaldo();
+// c1.Sacar(5874);
+// c1.ExibirSaldo();
 
 
 
